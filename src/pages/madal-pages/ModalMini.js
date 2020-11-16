c99e39fbf44558f232156a61695906bd8bd4f7ce
bambambigalo <kri69rus2016@yandex.ru>
2020-11-16 23:04:27 +0300

    2 files modified

--- src/pages/madal-pages/ModalMini.js
+++ src/pages/madal-pages/ModalMini.js
@@ -4,6 +4,14 @@ import ReactDOM from "react-dom";
 
 export default function ModalMini(props) {
   const { onClose } = props;
+  let colors = [
+    {white : 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-mini-white-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1604343707000'},
+    {black : 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-mini-black-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1604343705000'},
+    {blue : 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-mini-blue-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1604343706000'},
+    {green : 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-mini-green-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1604343706000'},
+    {red :  "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-mini-red-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1604343707000"}
+    
+  ]
 
   return ReactDOM.createPortal(
     <div>
@@ -17,7 +25,13 @@ export default function ModalMini(props) {
           </div>
 
           <div className="content">
-            <div className="view" />
+          <div className="view">
+          {colors.map (color => {
+            return (
+              <div  key={color.red} > {color.red} </div>
+            )
+          })}
+            </div>
             <div className="colors">
               <h2> Выберите цвет</h2>
               <button className="button-color">Графитовый</button>
